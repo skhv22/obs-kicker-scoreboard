f6058863b81afb3f9922fbd2e4f3102d1acec6e7
Alexey Sukhov <sontax2007@icloud.com>
2026-03-20 13:10:12 +0100

    update Firebase configuration with production credentials

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,9 @@
 window.FIREBASE_CONFIG = {
-  apiKey: "PASTE_HERE",
-  authDomain: "PASTE_HERE",
-  databaseURL: "PASTE_HERE",
-  projectId: "PASTE_HERE",
-  storageBucket: "PASTE_HERE",
-  messagingSenderId: "PASTE_HERE",
-  appId: "PASTE_HERE"
+  apiKey: "AIzaSyCUNyYojkhMiVt5TLSN-7HpqYAYG3wejJs",
+  authDomain: "obs-kicker-scoreboard.firebaseapp.com",
+  databaseURL: "https://obs-kicker-scoreboard-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "obs-kicker-scoreboard",
+  storageBucket: "obs-kicker-scoreboard.firebasestorage.app",
+  messagingSenderId: "741473495710",
+  appId: "1:741473495710:web:b729b55cb69fc6d2db698f"
 };
